5ba769118ba70323824016fbc6a472dd7d490723
Fernando PC <optimalleek@gmail.com>
2022-05-26 14:32:01 -0500

    detallando

--- mostrar.php
+++ mostrar.php
@@ -41,7 +41,7 @@ if ($inc) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <link rel="stylesheet" type="text/css" href="Estilos/estilos.css">
     <title>Base de Datos</title>
 </head>
 <body>
