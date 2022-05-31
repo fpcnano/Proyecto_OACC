50b83e75eb00331f5f12bf26b0a7a4a071a00336
Fernando PC <optimalleek@gmail.com>
2022-05-31 16:21:01 -0500

    Conexión BD Remota

--- db.php
+++ db.php
@@ -3,7 +3,10 @@
 session_start();
 
 $conn = mysqli_connect(
-    'localhost', 'root', '', 'examen'
+    'baujf7ul9g9yg6orxtbp-mysql.services.clever-cloud.com',
+    'ufarwjlsnovazodo',
+    'ex7ApPTHCFcYPinxGSjq',
+    'baujf7ul9g9yg6orxtbp'
 );
 
 ?>
