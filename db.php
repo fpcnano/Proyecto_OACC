96dd992559c3045dd1af6209b48e662d855cd783
Fernando PC <optimalleek@gmail.com>
2022-06-01 18:05:08 -0500

    Arreglando Conexión

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ session_start();
 
 $conn = mysqli_connect(
     'baujf7ul9g9yg6orxtbp-mysql.services.clever-cloud.com',
-    'ufarwjlsnovazodo',
+   'ufarwjlsnovazodo',
     'ex7ApPTHCFcYPinxGSjq',
     'baujf7ul9g9yg6orxtbp'
 );
